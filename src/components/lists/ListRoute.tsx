8fb267ce784ae4172401a6ab6c8bc751b2c2a74b
Llorens19 <llorenssorianodiego@gmail.com>
2025-01-30 02:20:06 +0100

    AÑADIR: Scroll al listRoutes

--- src/components/lists/ListRoute.tsx
+++ src/components/lists/ListRoute.tsx
@@ -6,26 +6,33 @@ import CardRoute from '@/components/cards/CardRoute';
 import { IRoute } from '@/interfaces/entities/route.interface';
 import { useRoutesQuery } from '@/queries/routes.query';
 import { IRouteFilters } from '@/interfaces/services/route/getRoutes.interface';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const ListRoute = () => {
   const searchParams = useSearchParams();
-  const [filters, setFilters] = useState<IRouteFilters>({ limit: 10, offset: 0 });
+  const [filters, setFilters] = useState<IRouteFilters>({ limit: 4, offset: 0 });
+  const [routesList, setRoutesList] = useState<IRoute[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const { data: routes, refetch } = useRoutesQuery(filters);
+
+
 
   useEffect(() => {
     const encodedFilters = searchParams.get('filters');
 
-
     if (encodedFilters) {
       try {
         const decoded = JSON.parse(atob(encodedFilters));
-        setFilters({...decoded, limit: 10, offset: 0});
+        setFilters({ ...decoded, limit: 4, offset: 0 });
+        setRoutesList([]);
       } catch (error) {
         console.error('Error decoding filters:', error);
       }
     }
   }, [searchParams]);
 
-  const { data: routes, refetch } = useRoutesQuery(filters);
+
 
 
 
@@ -34,11 +41,41 @@ const ListRoute = () => {
   }, [filters, refetch]);
 
 
+
+
+  useEffect(() => {
+    if (routes?.routes) {
+      setRoutesList((prevRoutes) => [...prevRoutes, ...routes.routes]);
+      setIsFetching(false);
+    }
+  }, [routes]);
+
+
+
+  const loadMoreRoutes = () => {
+    setIsFetching(true);
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      offset: (prevFilters.offset ?? 0) + (prevFilters.limit ?? 4),
+    }));
+  };
+
+
+
+
   return (
     <div className="flex-1 overflow-y-auto pr-4">
-      {routes?.routes.map((route: IRoute) => (
-        <CardRoute key={route.idRoute} route={route} />
-      ))}
+      <InfiniteScroll
+        dataLength={routesList.length}
+        next={loadMoreRoutes}
+        hasMore={!isFetching && (routes?.routes?.length ?? 0) > 0}
+        loader={<div className="text-center text-white py-4">Cargando... </div>}
+        endMessage={<div className="text-center text-white py-4">No hay más rutas</div>}
+      >
+        {routesList.map((route) => (
+          <CardRoute key={route.idRoute} route={route} />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 };
