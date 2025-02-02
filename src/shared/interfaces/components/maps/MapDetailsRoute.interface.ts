81386cd9ee75fcecbd549211e181d21ac7591a27
Llorens19 <llorenssorianodiego@gmail.com>
2025-02-02 13:31:27 +0100

    AÑADIR: Estadística altitud details

---
+++ src/shared/interfaces/components/maps/MapDetailsRoute.interface.ts
@@ -0,0 +1,5 @@
+import L from 'leaflet';
+
+export interface IMapDetailsRouteParams {
+  coordinates: L.LatLngTuple[];
+}
--- src/shared/interfaces/entities/route.interface.ts
+++ src/shared/interfaces/entities/route.interface.ts
@@ -1,9 +1,11 @@
+import { ICoordenate } from '@/shared/interfaces/entities/coordinate.interface';
 import { IImagesRoutes } from '@/shared/interfaces/entities/imagesRoutes.interface';
 import { Routes } from '@prisma/client';
 
 
-export interface IRoute extends Routes {
+export interface IRoute extends Omit<Routes, 'coordinates' | 'imagesRoutes'> {
   imagesRoutes?: IImagesRoutes[];
+  coordinates?: ICoordenate[];
 };
 
 export interface IRoutes {
