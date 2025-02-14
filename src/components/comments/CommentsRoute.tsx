f427aca9d5fed891629eca881557bb51dcf7a282
Llorens19 <llorenssorianodiego@gmail.com>
2025-02-14 11:47:07 +0100

    CORREGIR: Ocultar CommentCreate a usuarios no logeados

--- src/components/comments/CommentsRoute.tsx
+++ src/components/comments/CommentsRoute.tsx
@@ -2,12 +2,13 @@
 import CommentCard from '@/components/comments/CommentCard';
 import CommentCreate from '@/components/comments/CommentCreate';
 import { useRuteCommentsQuery } from '@/reactQuery/queries/comments.query';
+import { useGetUserQuery } from '@/reactQuery/queries/user.query';
 import { ICommentsRouteProps } from '@/shared/interfaces/components/comments/CommentsRoute.interface';
 import { IComment } from '@/shared/interfaces/entities/comment.interface';
 
 const CommentsRoute = ({ idRoute }: ICommentsRouteProps) => {
-
   const { data, isLoading } = useRuteCommentsQuery(idRoute);
+  const { data: userLogged } = useGetUserQuery();
 
   if (data === undefined) {
     return <div>Loading...</div>;
@@ -18,18 +19,12 @@ const CommentsRoute = ({ idRoute }: ICommentsRouteProps) => {
 
   return (
     <div className="flex flex-col gap-4 w-3/5 mx-auto my-8">
+      {userLogged && <CommentCreate idRoute={idRoute} />}
 
-      <CommentCreate idRoute={idRoute}/>
-
-
-      {comments && comments.map((comment: IComment) => (
+      {comments &&
+        comments.map((comment: IComment) => (
           <CommentCard comment={comment} key={comment.idComment} />
         ))}
-
-
-
-
-
     </div>
   );
 };
