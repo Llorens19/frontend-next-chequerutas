52c16fa36bb06bdb6b3bc299e3dee45328aa1a08
Llorens19 <llorenssorianodiego@gmail.com>
2025-03-17 21:30:14 +0100

    AÑADIR: Estructura básica del profile

--- src/components/lists/ListFavoritesProfile.tsx
+++ src/components/lists/ListFavoritesProfile.tsx
@@ -1,25 +1,33 @@
 import CardRoute from '@/components/cards/CardRoute';
 import CardRouteSkeleton from '@/components/cards/skeletons/CardRouteSkeleton';
+import CardRouteMobile from '@/compontesPhone/cards/CardRouteMobile';
+import useMobile from '@/hooks/useMobile.hook';
 import { useFavoriteUserQuery } from '@/reactQuery/queries/favorites.query';
 
 const ListFavoritesProfile = () => {
-
   const { data: favorites, isLoading } = useFavoriteUserQuery();
-
+  const isMobile = useMobile();
 
   return (
     <div className="flex-1 overflow-y-auto">
-      {!isLoading &&favorites?.favorites.map((favorite) => (
-          <CardRoute route={favorite.route!} key={favorite.idFavorite}/>
-      ))}
+      {!isLoading &&
+        favorites?.favorites.map((favorite) =>
+          isMobile ? (
+            <CardRouteMobile
+              route={favorite.route!}
+              key={favorite.idFavorite}
+            />
+          ) : (
+            <CardRoute route={favorite.route!} key={favorite.idFavorite} />
+          )
+        )}
       {isLoading && <CardRouteSkeleton />}
       {!isLoading && favorites?.favorites.length === 0 && (
-        <div className="text-center text-text1 py-4  text-xl">No tienes rutas guardadas</div>
+        <div className="text-center text-text1 py-4  text-xl">
+          No tienes rutas guardadas
+        </div>
       )}
     </div>
-
-
-
   );
 };
 
--- src/components/lists/ListRoteProfile.tsx
+++ src/components/lists/ListRoteProfile.tsx
@@ -1,19 +1,29 @@
 'use client';
 
 import CardRoute from '@/components/cards/CardRoute';
+import CardRouteMobile from '@/compontesPhone/cards/CardRouteMobile';
+import useMobile from '@/hooks/useMobile.hook';
 import { IListRouteProfileProps } from '@/shared/interfaces/components/lists/ListRouteProfile.interface';
 
-const ListRouteProfile = ({routes}: IListRouteProfileProps) => {
+const ListRouteProfile = ({ routes }: IListRouteProfileProps) => {
+  const isMobile = useMobile();
 
   return (
     <div className="flex-1 overflow-y-auto">
-        {routes.map((route) => (
+      {routes.map((route) =>
+        isMobile ? (
+          <CardRouteMobile key={route.idRoute} route={route} />
+        ) : (
           <CardRoute key={route.idRoute} route={route} />
-        ))}
+        )
+      )}
 
       {routes.length === 0 && (
-        <div className="text-center text-text1 py-4  text-xl">No tienes rutas guardadas</div>
+        <div className="text-center text-text1 py-4  text-xl">
+          No hay rutas
+        </div>
       )}
+
     </div>
   );
 };
